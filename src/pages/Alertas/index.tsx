5059720eb5e66aa18792c4bdea534614302bdd2e
Julio <juliopm142@gmail.com>
2023-09-22 20:01:08 -0300

    correcao/padronizacao das paginas de alertas, informacoes e estacoes

--- src/pages/Alertas/index.tsx
+++ src/pages/Alertas/index.tsx
@@ -45,7 +45,7 @@ const Alertas: React.FC = () => {
                     <div className='Alertas-container'>
                         <details className='details'>
                                 <summary className='summary'>
-                                    Nao tem alertas
+                                    Não tem alertas
                                 </summary>
                             </details>
                     </div>
